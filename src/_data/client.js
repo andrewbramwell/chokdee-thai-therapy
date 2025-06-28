56cd8503b8b4bcc10f8d77f11048e14d93a83a8b
andrewbramwell <andy@voucherstore.io>
2025-06-28 08:54:16 +0100

    first commit

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,23 +1,23 @@
 module.exports = {
-    "name": "Splendid Sites Web Designs",
-    "email": "help@splendidsites.com",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
-    "openingHours": "Mon-Sun: 9am-9pm",
-    "onlineBooking": "https://www.facebook.com/",
+    "name": "Chokdee Thai Therapy",
+    "email": "chokdeethaitherapy@gmail.com",
+    "phoneForTel": "+447958229813",
+    "phoneFormatted": "07958 229813",
+    "openingHours": "Mon-Fri: 10am-7pm, Sat: 3.30pm-7pm",
+    "onlineBooking": "https://chokdeethaitherapy.setmore.com/",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+        "lineOne": "77 Queen St",
+        "lineTwo": "",
+        "city": "Normanton",
+        "state": "West Yorkshire",
+        "zip": "WF6 1AF",
+        "country": "United Kingdom",
+        "mapLink": "https://maps.app.goo.gl/866FWxeEERejDbR8A"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
         "instagram": "https://www.instagram.com/",
-        "googleReview": "",
+        "googleReview": "https://g.page/r/Carswx-vZttxEBM/review",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
     "domain": "https://www.example.com"
